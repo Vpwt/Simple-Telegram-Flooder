c3b0a153317426eead0615bce9655e5660432443
Vpwt <46924386+Vpwt@users.noreply.github.com>
2021-06-18 20:46:44 +0530

    Update teleflood.py

--- teleflood.py
+++ teleflood.py
@@ -2,14 +2,13 @@ import webbrowser
 import time
 import keyboard
 
-weblink = input(print("input weblink of telegram user/group\t"))
-webbrowser.open(weblink, new=1, autoraise=True)
-time.sleep(100)
-
-msg = input(print("enter a message you want to flood\t"))
+url = input("Input Weblink Of Telegram User or Group >> ")
+msg = input("Enter A Message You Want To Flood >> ")
+webbrowser.open(url, new=1, autoraise=True)
+time.sleep(20)
 
 while True:
     keyboard.write(msg)
     time.sleep(1)
     keyboard.press_and_release('enter')
-    time.sleep(2)
+    time.sleep(0.2)
